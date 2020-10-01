b3b978a431de389dabfbc76c2791ab515fd9196f
Mica Maldonado <mica.04m@gmail.com>
2020-09-30 20:21:58 -0400

    agregando rutas para Type

--- src/App.js
+++ src/App.js
@@ -25,6 +25,10 @@ function AppMenu() {
         <Menu.Item key="tasks:1"><Link to="/tasks">Tasks</Link></Menu.Item>
         <Menu.Item key="tasks:2"><Link to="/tasks/new">New Task</Link></Menu.Item>
       </SubMenu>
+      <SubMenu key="types" icon={<AppstoreOutlined />} title="Types">
+        <Menu.Item key="types:1"><Link to="/types">Tasks</Link></Menu.Item>
+        <Menu.Item key="types:2"><Link to="/types/new">New Type</Link></Menu.Item>
+      </SubMenu>
       <Menu.Item key="about" icon={<AppstoreOutlined />}>
         <Link to="/about">About</Link>
       </Menu.Item>
@@ -33,6 +37,19 @@ function AppMenu() {
 }
 
 function TaskRoutes({ match }) {
+  return (
+    <>
+      <Route exact path={`${match.path}/new`} component={TaskForm} />
+      <Route
+        exact
+        path={`${match.path}/edit/:taskId`}
+        component={TaskForm}
+      />
+      <Route exact path={`${match.path}/`} component={TaskList} />
+    </>
+  );
+}
+function TypeRoutes({ match }) {
   return (
     <>
       <Route exact path={`${match.path}/new`} component={TaskForm} />
@@ -64,6 +81,7 @@ function App() {
                     <Route path="/about" component={About} />
                     {/* Hacemos esto porque tasks tiene subrutas */}
                     <Route path="/tasks" component={TaskRoutes} />
+                    <Route path="/types" component={TypeRoutes} />
                   </>
               </div>
           </Content>
